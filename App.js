7fe51b0fec7ebf6dd6b14952a954f61c764ed6f3
gvishnu98 <gvishnu641@gmail.com>
2020-12-24 23:12:09 +0530

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button,TouchableOpacity,Alert,Image} from 'react-native';
+import *as ImagePicker from 'expo-image-picker';
+import *as Permissions from 'expo-permissions';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = ()=>{
+
+  const [selectedImage,setSelectedImage] = React.useState(null);
+
+  let pickfromcamera = async()=>{
+    let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+      if(permissionResult.granted === false){
+        alert('you need to give up permission to work');
+        return;
+      }
+          let data = await ImagePicker.launchCameraAsync();
+          if(data.cancelled === true){
+            return;
+          }
+
+          setSelectedImage({localUri: data.uri});
+        };
+
+        if(selectedImage !== null){
+          return(
+            <View style={styles.container}>
+              <Image
+                 source={{uri:selectedImage.localUri}}
+                 style = {styles.thumbnail}
+              />      
     </View>
-  );
+          )}
+
+          return(
+            <View>
+              <TouchableOpacity style={{backgroundColor:'#0C948A',borderRadius:10,width:300,height:45,alignItems:'center',marginBottom:100}} onPress={() => pickfromcamera()}>
+        <Text style={{fontSize:16,fontWeight:"bold",color:'#fff',marginTop:10}}>camera</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto" />
+            </View>
+
+          )
+  
+ 
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +56,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  thumbnail: {
+    width:400,
+    height:400,
+    resizeMode:'contain'
+  }
 });
